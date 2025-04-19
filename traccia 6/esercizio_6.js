d4860b42e7f77db01866da6ed122678670fa76d4
Michael Mammino <famigliamammino5@libero.it>
2025-04-20 00:40:23 +0200

    fine

--- traccia 6/esercizio_6.js
+++ traccia 6/esercizio_6.js
@@ -7,4 +7,23 @@
 
 // trovare il numero di volte (occorrenze) in cui si ripetono ogni singola vocale.
 
-var msg = "ciao mamma guarda come mi divertoooo... con cantatuuu!!!";
+let msg = "ciao mamma guarda come mi divertoooo... con cantatuuu!!!";
+let i = 0;
+let repeat = [0,0,0,0,0];
+let vocal=['a','e','i','o','u'];
+
+while(i < msg.length) {
+    let j = 0;  
+    while(j < vocal.length) {
+        if(msg[i] == vocal[j]) {
+            repeat[j]++;
+        } 
+        j++;
+    }
+    i++;    
+}
+i = 0;
+while(i < vocal.length) {
+    console.log(`La lettera ${vocal[i]} si ripete ${repeat[i]} volte`)
+    i++;
+}
